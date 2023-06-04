98c7cb49da08436ad8b42e45560ece87c9051e0d
Matthew Aniskevich <matsvei@pxhealthcare.com>
2023-06-04 15:05:30 +0300

    init vite

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  server:{
+    port: 3000,
+  },
+  build:{
+    outDir: resolve(__dirname, 'dist'),
+  }
+})
