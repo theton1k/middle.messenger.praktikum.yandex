940e1753b696fc1999060123c0d7ca7a75753f1b
Anton Astreika <antonastreika@gmail.com>
2023-06-09 23:53:57 +0300

    fix file names, add start script

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,10 @@
 import Button from './Button/Button.tmpl.ts'
-import FormInput from './FormInput/FormInput.templ.ts'
-import Navlink from './Navlink/Navlink.templ.ts'
-import Avatar from './Avatar/Avatar.templ.ts'
-import UserInfoItem from './UserInfoItem/UserInfoItem.templ.ts'
-import MessageCounter from './MessageCounter/MessageCounter.templ.ts'
-import Separator from './Separator/Separator.templ.ts'
+import FormInput from './FormInput/FormInput.tmpl.ts'
+import Navlink from './Navlink/Navlink.tmpl.ts'
+import Avatar from './Avatar/Avatar.tmpl.ts'
+import UserInfoItem from './UserInfoItem/UserInfoItem.tmpl.ts'
+import MessageCounter from './MessageCounter/MessageCounter.tmpl.ts'
+import Separator from './Separator/Separator.tmpl.ts'
 
 
 export default {
